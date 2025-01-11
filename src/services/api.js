a343861cdbc8d9eaf3bd3926e49d99224e9360d9
Gabriel Duarte <gabriel.anpedu@gmail.com>
2025-01-11 01:01:45 -0300

    alteração URL com back-end

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://cadastro-de-usuarios-back-end.netlify.app'
 })
 
 export default api
